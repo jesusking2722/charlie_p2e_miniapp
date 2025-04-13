0de8a12d3e7d9b33d09660c895f39d0904c53442
jesusking2722 <jesusking2722@gmail.com>
2025-04-13 21:56:24 +0900

    modified:   application/views/template_view.php

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -27,8 +27,6 @@
     var user_surname=window.Telegram.WebApp.initDataUnsafe.user.last_name;
     var photo_url=window.Telegram.WebApp.initDataUnsafe.user.photo_url;
 
-    console.log("current user: ", window.Telegram.WebApp.initDataUnsafe.user);
-
     // var user_id=7712637143;
     // var user_username="";
     // var user_name="Mura";
@@ -120,14 +118,8 @@ if($user_data->online+3<time()){
         $(window).on('load', function() {
             setTimeout(() => {
                 $('.ppreloader').fadeOut(1000);
-                var user_id=window.Telegram.WebApp.initDataUnsafe.user.id;
-                var user_username=window.Telegram.WebApp.initDataUnsafe.user.username;
-                var user_name=window.Telegram.WebApp.initDataUnsafe.user.first_name;
-                var user_surname=window.Telegram.WebApp.initDataUnsafe.user.last_name;
-                var photo_url=window.Telegram.WebApp.initDataUnsafe.user.photo_url;
-
                 console.log("current user: ", window.Telegram.WebApp.initDataUnsafe.user);
-                location.href = '/';
+                // location.href = '/';
             }, 3000);
         });
     </script>
